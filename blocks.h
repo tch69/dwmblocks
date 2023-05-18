548ea60a3119e797a2d0f49f3793ec01999a7b0d
tch69 <ifa26417@aol.com>
2023-05-19 01:18:43 +0700

    COLOURRRRRRRSSSSSSSSSS

--- blocks.h
+++ blocks.h
@@ -1,13 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{ " ⛅ ", 	"wttr", 10, 0 },
-	{ " 💽 ", "df -h | awk '/root/ {print $1\" \"$3\"/\"$2\" \"$5\" \"$4}'", 	1, 		0},
-	{ "🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		0},
-	{ "🔊 ", "amixer get Master | awk '/Mono:/ {print $4}' | tr -d '[][]'", 1, 		0},
-	{"", "date '+%r [%d.%m.%Y]'",					1,		0},
+	{ " ⛅ ^c#e74218^", "wttr", 10, 0 },
+	{ " 💽 ^c#01a252^", "df -h | awk '/root/ { print $3\" \"$5}'", 	1, 		0},
+	{ " 🧠 ^c#dddd13^", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	1,		0},
+	{ " 🔊 ^c#3c8bef^", "amixer get Master | awk '/Mono:/ {print $4}' | tr -d '[][]'", 1, 		0},
+	{ " 📅 ^c#76aef4^", "date '+%d.%m.%Y'",					1,		0},
+	{ " 🕓 ^c#efefef^", "date '+%r  '",					1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
