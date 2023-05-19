223fc4279e0c44529693b0c71585da816a09da7d
tch69 <ifa26417@aol.com>
2023-05-19 15:44:19 +0700

    Optimisations

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{ " ⛅ ", "wttr", 10, 0 },
+	{ "  ⛅ ", "wttr", 10, 0 },
 	{ "  ", "df -h | awk '/root/ { print $3\" \"$5}'", 	1, 		0},
 	{ "  ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	1,		0},
 	{ " 🔊 ", "amixer get Master | awk '/Mono:/ {print $4}' | tr -d '[][]'", 1, 		0},
