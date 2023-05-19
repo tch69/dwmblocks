d860717152fcc40d34eacdb7f3de6e27f75e2d19
tch69 <ifa26417@aol.com>
2023-05-19 12:06:34 +0700

    Colours suck; back to simplicity

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{ " ⛅ ^c#e74218^", "wttr", 10, 0 },
-	{ " 💽 ^c#01a252^", "df -h | awk '/root/ { print $3\" \"$5}'", 	1, 		0},
-	{ " 🧠 ^c#dddd13^", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	1,		0},
-	{ " 🔊 ^c#3c8bef^", "amixer get Master | awk '/Mono:/ {print $4}' | tr -d '[][]'", 1, 		0},
-	{ " 📅 ^c#76aef4^", "date '+%d.%m.%Y'",					1,		0},
-	{ " 🕓 ^c#efefef^", "date '+%r  '",					1,		0},
+	{ " ⛅ ", "wttr", 10, 0 },
+	{ "  ", "df -h | awk '/root/ { print $3\" \"$5}'", 	1, 		0},
+	{ "  ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	1,		0},
+	{ " 🔊 ", "amixer get Master | awk '/Mono:/ {print $4}' | tr -d '[][]'", 1, 		0},
+	{ " 📅 ", "date '+%d.%m.%Y'",					1,		0},
+	{ " 🕓 ", "date '+%r  '",					1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
